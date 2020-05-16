6954555eff4b6d872fc175f15cbd215d900079ef
takadamu <0rg68v3m326626f@gmail.com>
2020-05-17 06:35:51 +0900

    $key $value foreach文練習

--- index.php
+++ index.php
@@ -167,3 +167,12 @@ for ($i = 1; $i <= 1000; $i++) {
   echo $i.'<br>';
 }
 ?>
+
+<?php
+$scores = array('数学' => 70, '英語' => 90, '国語' => 80);
+
+// この下にforeach文を書いてください
+foreach ($scores as $key => $value) {
+  echo $key.'は'.$value.'点です。';
+}
+?>
