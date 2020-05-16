d58bbdbf78b5d1d17cfb033097ff5bb1987ea8fd
takadamu <0rg68v3m326626f@gmail.com>
2020-05-17 07:16:26 +0900

    strlen,count,rand文練習

--- index.php
+++ index.php
@@ -176,3 +176,22 @@ foreach ($scores as $key => $value) {
   echo $key.'は'.$value.'点です。';
 }
 ?>
+
+<?php
+$str = 'progate';
+
+// strlenを用いて$strの長さをechoしてください
+echo strlen($str);
+
+echo '<br>';
+
+$array = array('HTML', 'CSS', 'PHP');
+
+// countを用いて$arrayの要素数をechoしてください
+echo count($array);
+
+echo '<br>';
+
+// randを用いて10から15までのランダムな数字をechoしてください
+echo rand(10,15);
+?>
