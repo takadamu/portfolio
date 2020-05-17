08cdf1f461fd4fd5bd221a4c19d74106f778ca4a
takadamu <0rg68v3m326626f@gmail.com>
2020-05-18 06:27:17 +0900

    税込み計算

---
+++  practice.php
@@ -0,0 +1,18 @@
+<!-- 税込み計算 -->
+<?php
+$price = 1000;
+$taxRate = 0.08;
+echo '変数$priceの値: '.$price;
+echo '<br>';
+echo '変数$taxRateの値: '.$taxRate;
+echo '<br>';
+echo '-----';
+echo '<br>';
+
+// この下にコードを書いてください
+$sumTax = $price+$price*$taxRate;
+echo '税込価格は'.$sumTax.'円です';
+
+?>
+<!--  -->
+
